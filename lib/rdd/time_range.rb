49cc730c7925026f73cf6962cb8d1407e6293ce2
Richard Boldway <richard.boldway@gmail.com>
2017-05-08 09:12:23 -0500

    Add interaction of day and hour segments

--- lib/rdd/time_range.rb
+++ lib/rdd/time_range.rb
@@ -1,27 +1,36 @@
 module Rdd
 
+  # Activity for 1/1/2015 @ 3PM UTC   2015-01-01-15
+  # Activity for 1/1/2015             2015-01-01-{0..23}
+  # Activity for all of January 2015  2015-01-{01..30}-{0..23}
+  # yyyy-mm-dd-{dd..dd}-{hh..hh}
+
   class TimeRange
     attr_accessor :after, :before
     def initialize(after,before)
       @after = after
       @before = before
     end
-    def hh
-      after = @after.strftime("%H")
-      before = @before.strftime("%H")
-      return "{#{ after }..#{ before }}" unless @before > @after
-      return after if @after
-      return before if @before
-    end
     def dd
       after = @after.strftime("%d")
       before = @before.strftime("%d")
-      return "{#{ after }..#{ before }}" unless @before > @after
+      return "{#{ after }..#{ before }}" if @before.day > @after.day
       return after if @after
       return before if @before
     end
+    def hh
+      after = @after.strftime("%k").strip
+      before = @before.strftime("%k").strip
+      if @before.day <= @after.day
+        if @before.hour > @after.hour
+          return "-{#{ after }..#{ before }}"
+        end
+        return "-#{after}" if @after
+        return "-#{before}" if @before
+      end
+      end
     def composite
-      "#{@after.strftime("%Y-%m")}-#{dd}-#{hh}"
+      "#{@after.strftime("%Y-%m")}-#{dd}#{hh}"
     end
   end
 
